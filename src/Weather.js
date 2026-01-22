8f6f94a69d8e9956449a8a3a3dd4eeb6af0a0d69
lienta3 <lien.ta3@gmail.com>
2026-01-22 20:39:15 +1100

    Working city search engine

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import axios from "axios";
-import FormattedDate from "./FormattedDate";
+import CurrentWeatherInfo from "./CurrentWeatherInfo";
 import "./Weather.css";
 
 export default function Weather(props) {
-  let [city, setCity] = useState();
+  let [city, setCity] = useState(props.defaultCity);
   let [weatherData, setWeatherData] = useState({ ready: false });
 
-  function handleClick(response) {
+  function handleResponse(response) {
     let api_key = "b2a5adcct04b33178913oc335f405433";
     let api_url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${api_key}&units=metric`;
     console.log(response.data);
@@ -24,6 +24,18 @@ export default function Weather(props) {
       icon: response.data.condition.icon_url,
     });
   }
+
+  function search() {
+    let api_key = "b2a5adcct04b33178913oc335f405433";
+    let api_url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${api_key}&units=metric`;
+    axios.get(api_url).then(handleResponse);
+  }
+
+  function handleClick(event) {
+    event.preventDefault();
+    search();
+  }
+
   function updateCity(event) {
     setCity(event.target.value);
   }
@@ -55,151 +67,12 @@ export default function Weather(props) {
       <div className="container">
         <div className="weather-app">
           <header>{form}</header>
-          <main>
-            <div className="row">
-              <div className="col-sm-6">
-                <h1 className="weather-app-city" id="city">
-                  {weatherData.cityResult}
-                </h1>
-                <p className="weather-app-details">
-                  <span>
-                    <FormattedDate date={weatherData.date} />
-                  </span>{" "}
-                  <span id="description">{weatherData.condition}</span>
-                  <br />
-                  Humidity:{" "}
-                  <strong id="humidity">{weatherData.humidity} %</strong>, Wind:
-                  <strong id="windSpeed"> {weatherData.wind} km/h</strong>
-                </p>
-              </div>
-              <div className="col-sm-6 d-flex justify-content-end">
-                <img
-                  src={weatherData.icon}
-                  alt="weather condition icon"
-                  className="current-icon"
-                />
-                <span className="weather-app-temperature-value">
-                  {Math.round(weatherData.temperature)}
-                </span>
-                <span className="weather-app-temperature-unit">°C</span>
-              </div>
-            </div>
-            <div className="weather-forecast" id="forecast">
-              <div>
-                Tue
-                <img
-                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
-                  alt="weather condition icon"
-                />
-                <div className="weather-forecast-temperatures">
-                  <div
-                    className="
-                    weather-forecast-temperature"
-                  >
-                    28°
-                  </div>
-                  <div
-                    className="
-                    weather-forecast-temperature"
-                  >
-                    22°
-                  </div>
-                </div>
-              </div>
-              <div>
-                Tue
-                <img
-                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
-                  alt="weather condition icon"
-                />
-                <div className="weather-forecast-temperatures">
-                  <div
-                    className="
-                    weather-forecast-temperature"
-                  >
-                    28°
-                  </div>
-                  <div
-                    className="
-                    weather-forecast-temperature"
-                  >
-                    22°
-                  </div>
-                </div>
-              </div>
-              <div>
-                Tue
-                <img
-                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
-                  alt="weather condition icon"
-                />
-                <div className="weather-forecast-temperatures">
-                  <div
-                    className="
-                    weather-forecast-temperature"
-                  >
-                    28°
-                  </div>
-                  <div
-                    className="
-                    weather-forecast-temperature"
-                  >
-                    22°
-                  </div>
-                </div>
-              </div>
-              <div>
-                Tue
-                <img
-                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
-                  alt="weather condition icon"
-                />
-                <div className="weather-forecast-temperatures">
-                  <div
-                    className="
-                    weather-forecast-temperature"
-                  >
-                    28°
-                  </div>
-                  <div
-                    className="
-                    weather-forecast-temperature"
-                  >
-                    22°
-                  </div>
-                </div>
-              </div>
-              <div>
-                Tue
-                <img
-                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
-                  alt="weather condition icon"
-                />
-                <div className="weather-forecast-temperatures">
-                  <div
-                    className="
-                    weather-forecast-temperature"
-                  >
-                    28°
-                  </div>
-                  <div
-                    className="
-                    weather-forecast-temperature"
-                  >
-                    22°
-                  </div>
-                </div>
-              </div>
-            </div>
-          </main>
+          <CurrentWeatherInfo data={weatherData} />
         </div>
       </div>
     );
   } else {
-    let api_key = "b2a5adcct04b33178913oc335f405433";
-    let api_url = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${api_key}&units=metric`;
-    axios.get(api_url).then(handleClick);
-
+    search();
     return "Loading...";
   }
 }
